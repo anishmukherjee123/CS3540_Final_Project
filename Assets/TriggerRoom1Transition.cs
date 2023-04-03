e793594dc1c0a27ffd9ca38cca7d437c2f67f929
marlaaaaine <98628128+marlaaaaine@users.noreply.github.com>
2023-04-02 21:48:58 -0400

    added lava in tunnel ridges

--- Assets/TriggerRoom1Transition.cs
+++ Assets/TriggerRoom1Transition.cs
@@ -7,19 +7,23 @@ public class TriggerRoom1Transition : MonoBehaviour
     // Start is called before the first frame update
 
     public AudioClip wallDestroyedSFX;
+
+    int enemyCount;
     void Start()
     {
-        
+        enemyCount = LevelManager.enemiesInLevel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = LevelManager.enemiesInLevel;
     }
 
     private void OnCollisionEnter(Collision collider) {
-        if(collider.gameObject.CompareTag("Player")) {
+        
+        print("enemies in level: " + enemyCount);
+        if(collider.gameObject.CompareTag("Player") && enemyCount <= 0) {
             Destroy(gameObject);
             // play a sound effect and load to the next level
             AudioSource.PlayClipAtPoint(wallDestroyedSFX, Camera.main.transform.position);
