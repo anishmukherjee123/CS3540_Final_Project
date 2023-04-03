445c60fffb5cb4f7e28cf49e2c40a81c9dcb66d9
marlaaaaine <98628128+marlaaaaine@users.noreply.github.com>
2023-04-03 02:23:51 -0400

    worked on maynard behavior, made so it works with playerhit, and added signal to go through wall in room1

--- Assets/Scripts/TriggerRoom1Transition.cs
+++ Assets/Scripts/TriggerRoom1Transition.cs
@@ -10,6 +10,12 @@ public class TriggerRoom1Transition : MonoBehaviour
     public GameObject player;
 
     public float maxDistance = 50f;
+    public float spd = 5f;
+    public float length = 5f;
+
+    public Color shine = Color.white;
+
+    public GameObject signal;
 
     int enemyCount;
     void Start()
@@ -25,30 +31,40 @@ public class TriggerRoom1Transition : MonoBehaviour
 
         float distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position);
         print("Distance to player before if statement: " + distanceToPlayer);
-        if(distanceToPlayer <= maxDistance && enemyCount <= 0) {
+        if (distanceToPlayer <= maxDistance && enemyCount <= 0)
+        {
             print("Distance to player in if statement: " + distanceToPlayer);
             Renderer renderer = gameObject.GetComponent<Renderer>();
-            Color emissiveColor = Color.white;
-            float emissiveIntesnity = 10f;
-            //m_EmissiveObject.GetComponent<Renderer>().material.SetColor("_EmissiveColor", emissiveColor * emissiveIntensity);
-            renderer.material.SetColor("_EmissiveColor", emissiveColor * emissiveIntesnity);
+            // renderer.material.color = Color.Lerp(renderer.material.color, shine, Mathf.PingPong(Time.time * spd, length));
+            // Color emissiveColor = Color.white;
+            // float emissiveIntesnity = 10f;
+            // //m_EmissiveObject.GetComponent<Renderer>().material.SetColor("_EmissiveColor", emissiveColor * emissiveIntensity);
+            // renderer.material.SetColor("_EmissiveColor", emissiveColor * emissiveIntesnity);
+            // GameObject[] signals = GameObject.FindGameObjectsWithTag("Signal");
+            // foreach(GameObject signal in signals) {
+            //     signal.SetActive(true);
+            // }
+            //GameObject signal = GameObject.FindGameObjectWithTag("Signal");
+            signal.SetActive(true);
         }
         // could calcuate the distance between the player and the endpt
         // when the player is within a certain distance
         // give them some kind of signal (make the wall glow or an arrow)
-        
+
     }
 
-    private void OnCollisionEnter(Collision collider) {
-        
+    private void OnCollisionEnter(Collision collider)
+    {
+
         print("enemies in level: " + enemyCount);
-        if(collider.gameObject.CompareTag("Player") && enemyCount <= 0) {
+        if (collider.gameObject.CompareTag("Player") && enemyCount <= 0)
+        {
             gameObject.SetActive(false);
             // play a sound effect and load to the next level
             AudioSource.PlayClipAtPoint(wallDestroyedSFX, Camera.main.transform.position);
             // level has been beat, load to next level
             GameObject.FindObjectOfType<LevelManager>().LevelBeat();
-            
+
         }
     }
 }
